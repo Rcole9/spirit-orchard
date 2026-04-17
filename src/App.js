a1baa7017c824ad8ea0582b00c533ab640e06263
Rachel Cole <rcole9@student.ccp.edu>
2026-04-17 01:33:56 +0000

    new things

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 40 }}>
+      <h1>🌱 Spirit Orchard 🌱</h1>
+      <p>A cozy farming and spirit-befriending game prototype.</p>
+      <p>Start building your orchard and befriend gentle spirits!</p>
+    </div>
+  );
+}
+
+export default App;
