4b1cb6b8643c3b450b4e68022d1c0726a588e705
Rachel Cole <rcole9@student.ccp.edu>
2026-04-18 00:11:45 +0000

    feat: update tilemap to include dungeon and wall tiles with corresponding images

--- src/assets/tilemap.js
+++ src/assets/tilemap.js
@@ -1,13 +1,13 @@
 // Simple tilemap for a 5x5 world
 // 0 = grass, 1 = path, 2 = crop, 3 = tree, 4 = player, 5 = spirit
 // 0 = grass, 1 = path, 2 = crop, 3 = tree, 4 = player, 5 = spirit
-// 6 = farm prop, 7 = furniture prop, 8 = pixel tree, 9 = meat, 10 = pan, 11 = alt pixel tree, 12 = vegetation, 13 = tools, 14 = third pixel tree, 15 = rocks, 16 = resources, 17 = dungeon props, 18 = esoteric, 19 = shadows, 20 = fourth pixel tree, 21 = bonfire, 22 = fifth pixel tree, 23 = building floor, 24 = building roof, 25 = building wall, 26 = building props, 27 = building shadows, 28 = water tile
+// 6 = farm prop, 7 = furniture prop, 8 = pixel tree, 9 = meat, 10 = pan, 11 = alt pixel tree, 12 = vegetation, 13 = tools, 14 = third pixel tree, 15 = rocks, 16 = resources, 17 = dungeon props, 18 = esoteric, 19 = shadows, 20 = fourth pixel tree, 21 = bonfire, 22 = fifth pixel tree, 23 = building floor, 24 = building roof, 25 = building wall, 26 = building props, 27 = building shadows, 28 = water tile, 29 = dungeon tile, 30 = wall tile, 31 = wall variation
 export const TILEMAP = [
   [0, 0, 1, 18, 15],
   [0, 2, 9, 3, 12],
   [1, 6, 23, 26, 21],
-  [13, 24, 8, 27, 11],
-  [16, 19, 25, 28, 22],
+  [13, 24, 29, 27, 11],
+  [16, 30, 25, 28, 31],
 ];
 
 export const TILE_IMAGES = {
@@ -38,6 +38,9 @@ export const TILE_IMAGES = {
  26: process.env.PUBLIC_URL + '/assets/images/structures/Buildings/Props.png', // building props
  27: process.env.PUBLIC_URL + '/assets/images/structures/Buildings/Shadows.png', // building shadows
  28: process.env.PUBLIC_URL + '/assets/images/tilesets/Water_tiles.png', // water tile
+ 29: process.env.PUBLIC_URL + '/assets/images/tilesets/Dungeon_Tiles.png', // dungeon tile
+ 30: process.env.PUBLIC_URL + '/assets/images/tilesets/Wall_Tiles.png', // wall tile
+ 31: process.env.PUBLIC_URL + '/assets/images/tilesets/Wall_Variations.png', // wall variation
 };
 
 export const PLAYER_SPRITE = process.env.PUBLIC_URL + '/assets/kenney_tiny-town/Tiles/tile_0004.png';
